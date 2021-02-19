2cfec36eb2563c3190f5722ff9af91405015dcf4
Alexis Santana Cano <azk.alexis.com@gmail.com>
2021-02-19 10:10:48 +0100

    Creado menu de administración Refinado menú lateral Cambios generales

--- scripts/admin.js
+++ scripts/admin.js
@@ -4,24 +4,28 @@ document.addEventListener("DOMContentLoaded", () => {
     let showButton = document.getElementById("show-sidebar");
 
     //Comprobar si el ancho es menor de 768px para ocultar automáticamente el menú lateral.
-    if (window.innerWidth < 768) {
-        sidebar.classList.add("sidebar-hide");
-        showButton.style.display = "block";
-    } 
+    let mq = window.matchMedia("(max-width: 1160px)");
+    mq.addEventListener("change", (e) => {
+        (e.matches) ? ocultarMenu() : mostrarMenu();
+    })
 
     //Añadir evento al botón de ocultar el menú lateral
     hideButton.addEventListener("click", function () {
-        if (!sidebar.classList.contains("sidebar-hide")) {
-            sidebar.classList.add("sidebar-hide");
-            showButton.style.display = "block";
-        }
+        if (!sidebar.classList.contains("sidebar-hide")) ocultarMenu();
     });
 
     //Añadir evento al botón de mostrar el menú lateral
     showButton.addEventListener("click", function () {
-        if (sidebar.classList.contains("sidebar-hide")) {
-            sidebar.classList.remove("sidebar-hide");
-            showButton.style.display = "none";
-        }
+        if (sidebar.classList.contains("sidebar-hide")) mostrarMenu();
     });
+
+    function mostrarMenu() {
+        sidebar.classList.remove("sidebar-hide");
+        showButton.style.display = "none";
+    }
+
+    function ocultarMenu() {
+        sidebar.classList.add("sidebar-hide");
+        showButton.style.display = "flex";
+    }
 });
