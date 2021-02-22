5a0d74e3ecf89a1c039bdd4c7d8fd990e2598580
Alexis Santana Cano <azk.alexis.com@gmail.com>
2021-02-22 19:13:00 +0100

    Limpieza del repositorio. Quitados ficheros y código no usado. Actualizados colores para cumplir el las normas de contraste.

--- scripts/login.js
+++ scripts/login.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let login = document.getElementById("login-frame");
     let register = document.getElementById("register-frame");
 
-    //TEMP: Al hacer click en el botón de iniciar sesión llevar a la página de administración. Quitar una vez que se compruebe correctamente en el servidor el login.
+    //Al hacer click en el botón de iniciar sesión llevar a la página de administración.
     document.getElementById("login-try").addEventListener("click", (e) => {
         e.preventDefault();
         window.location.replace("administracion.html");
@@ -20,18 +20,13 @@ document.addEventListener("DOMContentLoaded", () => {
         activeForm = active.getElementsByTagName("form")[0];    //Obtener el formulario correspondiente al elemento que se va a mostrar.
         hiddenForm = hidden.getElementsByTagName("form")[0];
 
-        //TODO: Comprobar si se puede resumir esto metiendo algunas cosas en la clase ".oculto" de CSS.
         if (active.classList[0] == "oculto") {
-            active.style.flexGrow = 3;  //Aumentar el ancho del contenedor
-            active.getElementsByClassName("icon-overlay")[0].style.opacity = "0";   //Hacer visible
-            active.style.cursor = "default";    //Cambiar el cursor a la flecha por defecto
+            active.classList.add("activo");     //Añadir la clase CSS "activo"
             active.classList.remove("oculto");  //Quitar la clase CSS "oculto"
             activeForm.style.transform = "translate(0px)"; //Mover el formulario a la posición final
 
-            hidden.style.flexGrow = 1;
-            hidden.getElementsByClassName("icon-overlay")[0].style.opacity = "1";
-            hidden.style.cursor = "pointer";
-            hidden.classList.add("oculto");
+            hidden.classList.add("oculto");     //Añadir la clase CSS "oculto"
+            hidden.classList.remove("activo");  //Quitar la clase CSS "activo"
 
             if (window.innerWidth > 530) {
                 hiddenForm.style.transform = "translateX(" + (hidden.id == "register-frame" ? "-270" : "270") + "px)";
